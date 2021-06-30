806307744e167bcf377647dee211fa6d7835530d
PaTara43 <p040399@outlook.com>
2021-06-30 13:22:21 +0300

    rollback to o sending eth

--- src/nft_minter.py
+++ src/nft_minter.py
@@ -218,19 +218,6 @@ def callback(data: String, packagepath: str) -> bool:
     txn_receipt = w3.eth.waitForTransactionReceipt(txn_hash)
     rospy.loginfo("txn_receipt: " + str(txn_receipt))
 
-    # sleep before sending transaction to the shop
-    time.sleep(2)
-    rospy.loginfo("Waiting for  service to send transaction to the shop")
-    rospy.wait_for_service('send_eth')
-    rospy.loginfo("Calling the service")
-    try:
-        send_eth = rospy.ServiceProxy('send_eth', send_eth_srv)
-        res = send_eth(config["parameters"]["minter"], config["parameters"]["shop_address"],
-                       0.01, config["parameters"]["minter_seed"])
-        rospy.loginfo(f"Result: {res.success}")
-    except rospy.ServiceException as e:
-        rospy.logerr(f"Service call failed: {e}")
-
 
 def listener(packagepath: str) -> None:
     """
